cc6271616a821ac032c72fb147d9231a41769013
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-01-24 18:46:05 +0530

    All updated

--- Js/pract.js
+++ Js/pract.js
@@ -1,12 +1,11 @@
-// remove the duplicate from an array
-const arr = [1, 2, 3, 2];
-let result = [];
-const uniqueArray = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (!result.includes(arr[i])) {
-      result.push(arr[i]);
-    }
-  }
-  return result;
-};
-console.log(uniqueArray(arr));
+const a = 10;
+(function () {
+  console.log(a);
+  var a = 20;
+})();
+
+function myFun() {
+  console.log(a);
+  const a = 20;
+}
+myFun();
