7453bb3f00b19c5c621be30a04526c426916665a
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-02-11 17:20:51 +0530

    first probelem sovled

--- Js consept/pract.js
+++ Js consept/pract.js
@@ -1,1 +1,14 @@
-// 
+// caluclate the sum of the dgit in a given number
+// eg 123 = 6
+let num = 123;
+
+const sumofDigit = (num) => {
+  let sum = 0;
+  while (num > 0) {
+    let digit = num % 10;
+    sum += digit;
+    num = Math.floor(num / 10);
+  }
+  return sum;
+};
+console.log(sumofDigit(num));
