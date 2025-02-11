db6e1b66f9e76c695f7433cfe7438b6a845e6047
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-02-11 17:28:30 +0530

    some problem soveld

--- Js consept/pract.js
+++ Js consept/pract.js
@@ -1,14 +1,4 @@
 // caluclate the sum of the dgit in a given number
-// eg 123 = 6
-let num = 123;
-
-const sumofDigit = (num) => {
-  let sum = 0;
-  while (num > 0) {
-    let digit = num % 10;
-    sum += digit;
-    num = Math.floor(num / 10);
-  }
-  return sum;
-};
-console.log(sumofDigit(num));
+// some() and every() in js
+// to avoid reference what we can do
+// merge two string with alteranatively
