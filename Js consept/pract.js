f1960e5c827e24c086a51560e8cc91f4ee9e0cff
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-02-13 02:05:45 +0530

    updated

--- Js consept/pract.js
+++ Js consept/pract.js
@@ -1,4 +1,2 @@
-// caluclate the sum of the dgit in a given number
-// some() and every() in js
-// to avoid reference what we can do
-// merge two string with alteranatively
+// wrte a function to reverse a number
+let num = 123;
