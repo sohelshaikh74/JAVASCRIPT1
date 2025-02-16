d9c6a1f7b0d201a120edd23c107276c8e077ea64
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-02-17 01:38:26 +0530

    Done problem

--- Js consept/pract.js
+++ Js consept/pract.js
@@ -4,3 +4,11 @@
 // remove duplicate element from an array
 // sum of an array
 // fizz bubzz game
+// convert first letter in captial of each of word in javas
+
+let str = "hello world, javascript is very awesome";
+
+const firstLetterCap = (str) => {
+  let strArr = str.split(" ");
+};
+console.log(firstLetterCap(str));
