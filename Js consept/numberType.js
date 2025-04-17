45200743193ed203444265fb2f4c4616a3ee62ad
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-04-17 22:36:55 +0530

    fa;sdlk

--- Js consept/numberType.js
+++ Js consept/numberType.js
@@ -1,15 +1,6 @@
-// number are stroed in floating point in js like 45 it will store in like 45.0
-// (note : number are stored in js are floating point no metter if we spceify floating point or not)
-console.log(45 === 45.0);
+// count the occurence in the string
+let str = "hello";
+let letter = "l";
 
-let now = new Date();
-console.log(now.getTime());
-
-// let dateString = now.toISOString("");
-// console.log(dateString.split("T")[0]);
-
-let d1 = new Date(12000);
-console.log(d1);
-
-let d2 = new Date(24 * 60 * 60 * 1000);
-console.log(d2);
+const countOccu = (str) => {};
+console.log(countOccu());
