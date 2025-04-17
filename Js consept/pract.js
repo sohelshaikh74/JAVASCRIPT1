9cc65556f5a1b0800785cd562b20703ae11c7003
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-04-17 22:35:05 +0530

    new featrue added

--- Js consept/pract.js
+++ Js consept/pract.js
@@ -1,13 +1,6 @@
-// palindrome a given number
-let num = 121;
-num = num.toString();
-const palindrome = (num) => {
-  let reversed = "";
-  for (let i = num.length - 1; i >= 0; i--) {
-    reversed += num[i];
-  }
-  if (reversed === num) {
-    return true;
-  } else return false;
-};
-console.log(palindrome(num));
+// swap a number without using third variable
+let a = 2;
+let b = 6;
+a = a + b; //8
+b = a - b; //2
+a = a - b; //6
