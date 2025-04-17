03557606679d43d0a700e0ff2efdb9c56eded2cf
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-04-17 22:33:26 +0530

    one more pragram added

--- Js consept/pract.js
+++ Js consept/pract.js
@@ -1,8 +1,13 @@
-//  reverse  a given string
-let str = "hello";
-let reversed = "";
-for (let i = str.length - 1; i >= 0; i--) {
-  reversed += str[i];
-}
-
-console.log(reversed);
+// palindrome a given number
+let num = 121;
+num = num.toString();
+const palindrome = (num) => {
+  let reversed = "";
+  for (let i = num.length - 1; i >= 0; i--) {
+    reversed += num[i];
+  }
+  if (reversed === num) {
+    return true;
+  } else return false;
+};
+console.log(palindrome(num));
