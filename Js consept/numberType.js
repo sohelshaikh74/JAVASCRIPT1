40b97506b15842e005f78c57eebf9ea28483349f
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-04-17 23:11:02 +0530

    new problem sovled

--- Js consept/numberType.js
+++ Js consept/numberType.js
@@ -1,6 +1,10 @@
 // count the occurence in the string
 let str = "hello";
 let letter = "l";
-
-const countOccu = (str) => {};
-console.log(countOccu());
+let count = 0;
+for (let i = str.length - 1; i >= 0; i--) {
+  if (str[i] === letter) {
+    count++;
+  }
+}
+console.log(count);
