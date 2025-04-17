a7750cd6d4148eb666c8e9a6c415b920a17f64e4
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-04-17 22:25:03 +0530

    All updated

--- Js consept/pract.js
+++ Js consept/pract.js
@@ -1,7 +1,3 @@
-// reverse a string without built in funciton
-// check if a string is a palindrome or not
-// find the largest number in the given array
-// remove duplicate element from an array
-// sum of an array
-// fizz bubzz game
-// convert first letter in captial of each of word in javas
+const arr = ["john", "mark", "steve"]; //convert arary to string
+let result = arr.toString();
+console.log(result);
