5cad6f586158b058e1d1ebb41a1c4e80d27d1947
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-09-27 21:27:21 +0530

    new topic added findIndex() method

--- ArrayMethods/findIndex.js
+++ ArrayMethods/findIndex.js
@@ -1,2 +1,21 @@
 let numbers = [5, 12, 8, 130, 44]
-let index
+let index = numbers.findIndex((ele) => {
+    return ele > 10;
+})
+console.log("index", index)
+
+let users = [
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bob" },
+    { id: 3, name: "Charlie" },
+]
+
+//find the index of that user whose id is equal to two
+let indexNumber
+let indexUser = users.findIndex((ele) => indexNumber = ele.id === 2)
+console.log(indexUser)
+
+
+//indexOf()  
+let indexOfElement = numbers.indexOf(130)
+console.log(indexOfElement)
